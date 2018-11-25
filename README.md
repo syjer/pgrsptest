254d0edde14c37ca71c5b62ca8b90fca3fccc709
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-11-25 16:05:20 +0100

    test

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ Actuator url: http://localhost:8080/actuator
 Rest api:
 
 - http://localhost:8080/option/list
+
+
+TODO:
+ - check what additional role we need to grant (most likely on sequence)
+ - set the value dynamically from a request param
---
+++ src/main/java/com/syjer/pgrsptest/pgrsptest/TestAspect.java
@@ -0,0 +1,43 @@
+package com.syjer.pgrsptest.pgrsptest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Component
+@Aspect
+public class TestAspect {
+
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public TestAspect(NamedParameterJdbcTemplate jdbcTemplate) {
+        System.err.println("aspect yooo");
+        this.jdbcTemplate = jdbcTemplate;
+
+    }
+
+    @Around("within(com.syjer.pgrsptest.pgrsptest.*) && (@target(org.springframework.transaction.annotation.Transactional) || " +
+            " @annotation(org.springframework.transaction.annotation.Transactional))")
+    public Object setRoleAndVariable(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.err.println(joinPoint);
+
+        DataSource dataSource = jdbcTemplate.getJdbcTemplate().getDataSource();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        if(DataSourceUtils.isConnectionTransactional(connection, dataSource)) {
+            System.err.println("connection is transactional");
+            jdbcTemplate.update("set role application_user", new EmptySqlParameterSource());
+            jdbcTemplate.update("set local my.var = '1'", new EmptySqlParameterSource());
+        } else {
+            System.err.println("connection is not transactional");
+        }
+        return joinPoint.proceed();
+    }
+}
