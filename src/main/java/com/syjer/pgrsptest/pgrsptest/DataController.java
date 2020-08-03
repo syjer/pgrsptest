346e2b193b6d7de24b028dc46ae4207400fc6122
Sylvain Jermini <sylvain.jermini@syjer.com>
2020-08-03 19:53:02 +0200

    remove aspect, set local values in a custom DataSourceTransactionManager

--- src/main/java/com/syjer/pgrsptest/pgrsptest/DataController.java
+++ src/main/java/com/syjer/pgrsptest/pgrsptest/DataController.java
@@ -1,5 +1,6 @@
 package com.syjer.pgrsptest.pgrsptest;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,24 @@ import java.util.List;
 public class DataController {
 
     private final DataManager dataManager;
+    private final JdbcTemplate jdbcTemplate;
 
-    public DataController(DataManager dataManager) {
+    public DataController(DataManager dataManager, JdbcTemplate jdbcTemplate) {
         this.dataManager = dataManager;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
 
     @GetMapping("/option/list")
     public List<KeyValue> getAll() {
         return dataManager.findAll();
+    }
+
+    @GetMapping("/option/list-direct")
+    public List<KeyValue> getAllDirect() {
+        return jdbcTemplate.query("select * from application_option", (rs, i) ->
+                new KeyValue(rs.getLong("ao_id"), rs.getString("ao_key"), rs.getString("ao_value"))
+        );
     }
 
 
--- src/main/java/com/syjer/pgrsptest/pgrsptest/PgrsptestApplication.java
+++ src/main/java/com/syjer/pgrsptest/pgrsptest/PgrsptestApplication.java
@@ -2,11 +2,46 @@ package com.syjer.pgrsptest.pgrsptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
+@Configuration
 public class PgrsptestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PgrsptestApplication.class, args);
+	}
+
+
+	@Bean
+	public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+		return new CustomDataSourceTransactionManager(dataSource);
+	}
+
+
+	private static class CustomDataSourceTransactionManager extends DataSourceTransactionManager {
+		CustomDataSourceTransactionManager(DataSource dataSource) {
+			super(dataSource);
+		}
+
+		@Override
+		protected void prepareTransactionalConnection(Connection con, TransactionDefinition definition) throws SQLException {
+			super.prepareTransactionalConnection(con, definition);
+
+			try (Statement s = con.createStatement()) {
+				s.execute("set local role application_user");
+			}
+			try (Statement s = con.createStatement()) {
+				s.execute("set local my.var = '1'");
+			}
+		}
 	}
 }
